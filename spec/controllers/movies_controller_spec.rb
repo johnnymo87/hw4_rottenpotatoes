ffe8aaf9c60ae0652bd08e8e692ce962e884455e
johnnymo87 <johnnymo87@gmail.com>
2013-08-10 15:53:45 -0700

    syncing with chapter 8 preconditions

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'searching TMDb' do
+    it 'should call the model method that performs TMDb search' do
+      pending 'need to choose name for model method'
+    end
+    it 'should select the Search Results template for rendering' do
+      pending 'no template exists yet'
+    end
+    it 'should make the TMDb search results available to that template'
+  end
+end
