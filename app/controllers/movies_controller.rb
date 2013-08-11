f72e44974f947972f8fa7887287117b6e2f97dbc
johnnymo87 <johnnymo87@gmail.com>
2013-08-11 08:12:18 -0700

    Rspec for MoviesController

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -65,9 +65,7 @@ class MoviesController < ApplicationController
   end
 
   def search_tmdb
-    # hardwired to simulate failure
-    flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
-    redirect_to movies_path
+    @movies = Movie.find_in_tmdb(params[:search_terms])
   end
 
 end
---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,13 @@
+describe Movie do
+  it 'should include rating and year in full name' do
+    movie = FactoryGirl.build(:movie, :title => 'Milk', :rating => 'R')
+    movie.name_with_rating.should == 'Milk (R)'
+  end
+end
+
+# or if you mix in FactoryGirl's syntax methods (see FactoryGirl README):
+
+#describe Movie do
+#  subject { create :movie, :title => 'Milk', :rating => 'R' }
+#  its :name_with_rating { should == 'Milk (R)' }
+#end
